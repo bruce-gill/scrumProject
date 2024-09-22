a93fb84bace38de47f308012e03a068b4c0fb4de
Bruce Gill <brucebag10@gmail.com>
2024-09-21 19:50:44 -0600

    home page

--- helixsprints/urls.py
+++ helixsprints/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from . import views
 urlpatterns = [
+    path("", views.index, name="index"),
     path("scrum/", include("scrum.urls")),
     path("admin/", admin.site.urls),
 ]
